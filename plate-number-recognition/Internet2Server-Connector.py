024cafb405b0d3e1fda2a15d80c3d6c76a9c62a3
Georges Hachem <36544153+georgeshachem@users.noreply.github.com>
2019-05-14 23:18:53 +0300

    Update Internet2Server-Connector.py

--- plate-number-recognition/Internet2Server-Connector.py
+++ plate-number-recognition/Internet2Server-Connector.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import json
 import requests
+import ast
 
 mqtt_broker_address = "212.98.137.194"
 mqtt_port = 1883
@@ -12,21 +13,25 @@ def on_connect(client, userdata, flags, rc):
 
 
 def on_message(client, userdata, msg):
-    data = json.loads(msg.payload)
+    try:
+        data = msg.payload.decode('UTF-8')
+        data = ast.literal_eval(data)
+        print(data)
 
-    base_url = "http://10.81.16.113:3000/Detection/create"
-    headers = {
-        'Content-Type': "application/json"
-    }
-    json_payload = json.dumps(data)
-    r = requests.post(base_url, data=json_payload, headers=headers)
-    print("Status Code: " + r.status_code)
+        base_url = "http://172.16.115.81:4000/Detection/create"
+        headers = {
+            'Content-Type': "application/json"
+        }
+        r = requests.post(base_url, json=data, headers=headers)
+        print("Status Code: " + str(r.status_code))
+    except Exception as e:
+        print(e)
 
 
 if __name__ == "__main__":
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
-    client.username_pw_set("user", "bonjour")
+    client.username_pw_set("iotleb", "iotleb")
     client.connect(mqtt_broker_address, mqtt_port, 60)
     client.loop_forever()
